3e6cc00c78de5412468a0d3f3301da32930bb87d
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-09-19 10:45:31 -0500

    minor setup

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+
+document.getElementById("message").innerHTML="Hello World!";
+
+//Variables vs. Literals
+    // string literal: "Hello World"
+    let hello = "Hello world. How are you today?"
+    let el = document.getElementById("m2");
+
+    el.innerHTML = hello;
